5b62d5dc5b179f65d1a9f69f154eff27cc24fb04
mduraj <michalduraj6@gmail.com>
2014-11-20 23:53:32 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+inv<-matrix()   ## expected result - inverted x
+set<-function(g) {
+        x<<-g
+        inv<<-matrix()
+}       ## sets an input matrix
+        
+get<-function() {x} ## it returns our matrix x
+setinv<-function(y) {inv<<-y} ## sets inv as y
+getinv<- function() {inv} ## returns a result
+list(set=set,get=get,setinv=setinv,getinv=getinv) ##prints functions defined inside
+
 
 }
 
@@ -11,5 +22,13 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+       invv<-x$getinv()  ## named invv to distinguish invv and inv from previous function
+       if(!is.na(invv[1,1])) {  ## checks if result is either a numeric matrix or 1x1 matrix NA
+               message("getting cached data") ## if so it prints this message
+               return(invv) ## and returns inversed matrix
+       }
+       data<-x$get() ## if not we need to load our matrix
+       invv<-solve(data) ## and calculate its inverse...
+       x$setinv(invv) ## ...set inverse matrix also...
+       invv ##...and print this inverse
 }
